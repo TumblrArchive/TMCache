f8f1244726ece89f2a080b73378d9506ce55b5c6
Justin Ouellette <justin.ouellette@gmail.com>
2013-04-21 15:50:55 -0400

    first stab at some tests

--- tests/TMCacheTests/TMCacheTests.h
+++ tests/TMCacheTests/TMCacheTests.h
@@ -1,11 +1,3 @@
-//
-//  TMCacheTests.h
-//  TMCacheTests
-//
-//  Created by Justin Ouellette on 4/14/13.
-//  Copyright (c) 2013 Tumblr. All rights reserved.
-//
-
 #import <SenTestingKit/SenTestingKit.h>
 
 @interface TMCacheTests : SenTestCase
--- tests/TMCacheTests/TMCacheTests.m
+++ tests/TMCacheTests/TMCacheTests.m
@@ -1,32 +1,126 @@
-//
-//  TMCacheTests.m
-//  TMCacheTests
-//
-//  Created by Justin Ouellette on 4/14/13.
-//  Copyright (c) 2013 Tumblr. All rights reserved.
-//
-
 #import "TMCacheTests.h"
+#import "TMCache.h"
+
+NSString * const TMCacheTestName = @"TMCacheTest";
+NSTimeInterval TMCacheTestBlockTimeout = 5.0;
+
+@interface TMCacheTests ()
+@property (strong, nonatomic) TMCache *cache;
+@end
 
 @implementation TMCacheTests
+
+#pragma mark - SenTestCase -
 
 - (void)setUp
 {
     [super setUp];
     
-    // Set-up code here.
+    self.cache = [[TMCache alloc] initWithName:TMCacheTestName];
+    
+    STAssertNotNil(self.cache, @"test cache does not exist");
 }
 
 - (void)tearDown
 {
-    // Tear-down code here.
+    [self.cache removeAllObjects];
     
     [super tearDown];
 }
 
-- (void)testExample
+#pragma mark - Private Methods
+
+- (UIImage *)image
 {
-    STFail(@"Unit tests are not implemented yet in TMCacheTests");
+    static UIImage *image = nil;
+    
+    if (!image) {
+        NSError *error = nil;
+        NSURL *imageURL = [[NSBundle mainBundle] URLForResource:@"Default-568h@2x" withExtension:@"png"];
+        NSData *imageData = [[NSData alloc] initWithContentsOfURL:imageURL
+                                                          options:NSDataReadingUncached
+                                                            error:&error];
+        image = [[UIImage alloc] initWithData:imageData scale:2.f];
+    }
+
+    NSAssert(image, @"test image does not exist");
+
+    return image;
+}
+
+- (dispatch_time_t)timeout
+{
+    return dispatch_time(DISPATCH_TIME_NOW, (int64_t)(TMCacheTestBlockTimeout * NSEC_PER_SEC));
+}
+
+#pragma mark - Tests -
+
+- (void)testCoreProperties
+{
+    STAssertTrue([self.cache.name isEqualToString:TMCacheTestName], @"wrong name");
+    STAssertNotNil(self.cache.memoryCache, @"memory cache does not exist");
+    STAssertNotNil(self.cache.diskCache, @"disk cache doe not exist");
+}
+
+- (void)testDiskCacheURL
+{
+    BOOL isDir = NO;
+    BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:[self.cache.diskCache.cacheURL path] isDirectory:&isDir];
+
+    STAssertTrue(exists, @"disk cache directory does not exist");
+    STAssertTrue(isDir, @"disk cache url is not a directory");
+}
+
+- (void)testObjectSet
+{
+    NSString *key = @"key";
+    __block UIImage *image = nil;
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    
+    [self.cache setObject:[self image] forKey:key block:^(TMCache *cache, NSString *key, id object) {
+        image = (UIImage *)object;
+        dispatch_semaphore_signal(semaphore);
+    }];
+
+    dispatch_semaphore_wait(semaphore, [self timeout]);
+    
+    STAssertNotNil(image, @"object was not set");
+}
+
+- (void)testObjectGet
+{
+    NSString *key = @"key";
+    __block UIImage *image = nil;
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    
+    [self.cache setObject:[self image] forKey:key];
+    
+    [self.cache objectForKey:key block:^(TMCache *cache, NSString *key, id object) {
+        image = (UIImage *)object;
+        dispatch_semaphore_signal(semaphore);
+    }];
+
+    dispatch_semaphore_wait(semaphore, [self timeout]);
+    
+    STAssertNotNil(image, @"object was not got");
+}
+
+- (void)testObjectRemove
+{
+    NSString *key = @"key";
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    
+    [self.cache setObject:[self image] forKey:key];
+    
+    [self.cache removeObjectForKey:key block:^(TMCache *cache, NSString *key, id object) {
+        dispatch_semaphore_signal(semaphore);
+    }];
+    
+    dispatch_semaphore_wait(semaphore, [self timeout]);
+    
+    id object = [self.cache objectForKey:key];
+    
+    STAssertNil(object, @"object was not removed");
 }
 
 @end
